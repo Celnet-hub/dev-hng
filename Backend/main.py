12a48594a0b27ee5be6ffbe58487cb75e9cf017e
Joseph Ologunja <68515821+Joseun@users.noreply.github.com>
2022-11-19 16:56:41 +0100

    dev hng

--- Backend/main.py
+++ Backend/main.py
@@ -3,12 +3,12 @@ import sys
 from fastapi import Depends, FastAPI, HTTPException
 from sqlalchemy.orm import Session
 
-from models import Account_statement
-from schemas import Account_pal
-from config.db import SessionLocal, engine
-Account_statement.Base.metadata.create_all(bind=engine)
+# from models import Account_statement
+# from schemas import Account_pal
+# from config.db import SessionLocal, engine
+# Account_statement.Base.metadata.create_all(bind=engine)
 from fastapi.middleware.cors import CORSMiddleware
-from routes import test_get_landing_page, get_landing_page
+from routes import get_landing_page
 
 
 app = FastAPI()
@@ -29,7 +29,6 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-app.include_router(test_get_landing_page.router)
 app.include_router(get_landing_page.router)
 
 
